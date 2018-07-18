4eddfafdc9fe5e2c991deaebe7b9e90b57b958a9
liumingming <mingmingsuper@gmail.com>
2018-07-18 10:45:07 +0800

    api 名字更新

--- app/src/main/java/com/hhmedic/android/hhdoctorvideodemo/receiver/Controller.java
+++ app/src/main/java/com/hhmedic/android/hhdoctorvideodemo/receiver/Controller.java
@@ -26,7 +26,7 @@ public class Controller extends BroadcastReceiver
 
         if (!TextUtils.isEmpty(command))
         {
-            HHDoctor.handUp();
+            HHDoctor.hangUp();
         }
     }
 }
